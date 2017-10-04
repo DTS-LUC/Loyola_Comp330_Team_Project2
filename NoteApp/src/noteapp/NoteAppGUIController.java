378c2423cc197921f8e40ef6042835352a3b878c
Donald Stolz <donaldtstolz@gmail.com>
2017-10-04 02:02:54 -0500

    Initial UI design

---
+++ NoteApp/src/noteapp/NoteAppGUIController.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author don
+ */
+package noteapp;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXListView;
+import com.jfoenix.controls.JFXTextArea;
+import com.jfoenix.controls.JFXTextField;
+import com.jfoenix.controls.JFXToggleButton;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class NoteAppGUIController {
+
+    @FXML
+    private void removeNote(ActionEvent e){
+        System.out.println("Remove button");
+    }
+    
+    @FXML
+    private void newNote(ActionEvent e){
+        System.out.println("New button");
+    }
+    
+    
+    @FXML
+    private JFXTextField searchField;
+    @FXML
+    private JFXComboBox<?> sortBox;
+    @FXML
+    private JFXTextField noteTitle;
+    @FXML
+    private JFXToggleButton favToggle;
+    @FXML
+    private JFXButton updateBtn;
+    @FXML
+    private JFXButton newBtn;
+    @FXML
+    private JFXButton removeBtn;
+    @FXML
+    private JFXListView<?> noteListViem;
+    @FXML
+    private JFXTextArea notePad;
+
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    } 
+}
