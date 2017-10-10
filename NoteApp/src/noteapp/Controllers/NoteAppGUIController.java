589d95de68d2ec76e5b5f5b6f9db0c695a24c014
msukan <31421218+msukan@users.noreply.github.com>
2017-10-10 18:37:45 -0500

    Add files via upload
    Added word count.

--- NoteApp/src/noteapp/Controllers/NoteAppGUIController.java
+++ NoteApp/src/noteapp/Controllers/NoteAppGUIController.java
@@ -1,3 +1,4 @@
+ lines (164 sloc)  4.56 KB
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -16,70 +17,173 @@ import com.jfoenix.controls.JFXListView;
 import com.jfoenix.controls.JFXTextArea;
 import com.jfoenix.controls.JFXTextField;
 import com.jfoenix.controls.JFXToggleButton;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.stage.DirectoryChooser;
+import noteapp.Model.Notes;
 
 public class NoteAppGUIController {
+		private Notes notes;
+// Lists;
+		private List<String> sorts = new ArrayList<>();
+		private List<String> noteList = new ArrayList<>();
+// JFX Objects
 		@FXML
 		private JFXTextField searchField;
 		@FXML
-		private JFXComboBox<?> sortBox;
+		private JFXComboBox<String> sortBox;
 		@FXML
 		private JFXTextField noteTitle;
 		@FXML
 		private JFXToggleButton favToggle;
+    @FXML
+		private JFXButton folderBtn;
 		@FXML
-		private JFXButton updateBtn;
+		private JFXButton saveBtn;
 		@FXML
 		private JFXButton newBtn;
 		@FXML
 		private JFXButton removeBtn;
 		@FXML
-		private JFXListView<?> noteListViem;
+		private JFXListView<String> noteListView;
 		@FXML
 		private JFXTextArea notePad;
-		//TODO Create global models so they can be called by controller methods
 
+// ActionEvent Methods
     @FXML
-    private void removeNote(ActionEvent e){
-        System.out.println("Remove button");
-        // TODO create a function that deletes files
-        // Needs to get file name from 'noteTitle'
-    }
-
-    @FXML
-    private void newNote(ActionEvent e){
-        System.out.println("New button");
-	// TODO create a function that creates new files
-        // Asks user to confirm save
-        // Brings up a blank note
-        // Needs to take value from 'notePad' textArea
-        // Also saves the filename from 'noteTitle'
-    }
-
-    @FXML // Setup within the FXML file. Called on button click
-    private void updateNote(ActionEvent e){
-        //Once button is clicked method begins
-        System.out.println("Update button");
-        // To get text values call the name of the component
-        //  with '.getValue()'
-        System.out.println(notePad.getText());
-        // Program will then need to invoke the model class to update the values
+    private void removeNote(ActionEvent e) throws IOException{
+				// TODO Confirmation box
+        // Get noteName from 'noteTitle'
+				String noteName = noteTitle.getText();
+				// Sends noteName to removeNote in Notes
+				notes.removeNote(noteName);
+				//Refresh noteListView
+				newNote();
+				updateNoteList();
     }
 
     @FXML
-    private void toggleFav(ActionEvent e){
-        System.out.println("Favorite's toggle");
-	// TODO toggles favorite value on note data.
-        // Need to svae this somewhere in the file
+    private void newNote(){
+      System.out.println("New note");
+			noteTitle.setText("Note Title");
+			notePad.setText(null);
     }
 
+    @FXML
+    private void saveNote() throws FileNotFoundException{
+				// Get noteName from 'noteTitle'
+				String noteName = noteTitle.getText();
+				// Get content from 'notePad'
+				String content = notePad.getText();
+				// Sends noteName to updateNote in Notes
+				notes.updateNote(noteName, content);
+				// Update list
+				updateNoteList();
+    }
+
+    @FXML
+    private void toggleFav(){
+        System.out.println("FAV toggle");
+				// TODO toggles favorite value on note data.
+        // Need to save this somewhere in the file
+    }
+
+		public void displayNote(String noteName){
+					noteTitle.setText(noteName);
+					notePad.setText(notes.getNote(noteName));
+		}
+
+		public void updateNoteList(){
+			// Get list of note names from "Notes"
+			noteList = FXCollections.observableArrayList(notes.allNames());
+			noteListView.getItems().clear();
+			// Add Items noteListView
+			noteListView.getItems().addAll(noteList);
+			// Add event listeners to list
+			noteListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+	      @Override
+	      public void changed(ObservableValue<? extends String> ov, String old_val, String selected) {
+	          displayNote(selected);
+			  }
+		  });
+		}
+		public void displayKeyword(){
+			
+		}
+		public void setKeywordList(char cmd, ArrayList<String> identifiers){
+			if (identifiers == null) {
+				// Get all
+				switch (cmd){
+
+                                        case '&' :                                 // new part
+						notes.allWords();
+						break;
+
+					case '@' :
+						notes.allMentions();
+						break;
+					case '#' :
+						notes.allTopics();
+						break;
+					case '!' :
+						notes.allIDs();
+						break;
+					default:
+						updateNoteList();
+				}
+			}
+			else{
+				// Get selectively
+				switch (cmd){
+                                        case '&' :                               // new part
+						notes.findWords();
+						break;
 
 
-    public void initialize(URL url, ResourceBundle rb) {
+					case '@' :
+						notes.findMentions();
+						break;
+					case '#' :
+						notes.findTopics();
+						break;
+					case '!' :
+						notes.findIDs();
+						break;
+					default:
+						updateNoteList();
+					}
+				}
+			}
 
-				// Will need to send dirPath to model
+    public void initialize() {
+        // Open directory chooser
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        directoryChooser.setTitle("Select a folder");
+        File selectedDirectory = null;
+				// Loop to make sure directory is chosen
+				while(selectedDirectory == null){
+					selectedDirectory = directoryChooser.showDialog(null);
+				}
+
+        // Create notes - send directory path
+        this.notes = new Notes(selectedDirectory.getAbsolutePath());
+        // Set List View Items
+				updateNoteList();
+
+				// Set values for sortBox
+				sorts	= FXCollections.observableArrayList("","@", "#", "!");
+        sortBox.getItems().addAll(sorts);
+
     }
 }
