b82231e63e38cb53e8309ed156e46f1ff226ab76
donald-stolz <donaldtstolz@gmail.com>
2017-10-11 08:29:04 -0500

    Travis Work

--- NoteApp/src/noteapp/Controllers/NoteAppGUIController.java
+++ NoteApp/src/noteapp/Controllers/NoteAppGUIController.java
@@ -118,7 +118,7 @@ public class NoteAppGUIController {
 		  });
 		}
 		public void displayKeyword(){
-			
+
 		}
 		public void setKeywordList(char cmd, ArrayList<String> identifiers){
 			if (identifiers == null) {
@@ -144,13 +144,8 @@ public class NoteAppGUIController {
 						//notes.findMentions();
 						break;
 					case '#' :
-<<<<<<< HEAD
 						//notes.findTopics();
 						break;
-=======
-						notes.findTopics();
-						//break;
->>>>>>> 857bd7b83280365d5276174a4a888f3f41e58dcd
 					case '!' :
 						//notes.findIDs();
 						break;
